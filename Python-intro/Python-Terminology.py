83d7fd4cb337305bb9851ed536ddd06f98141175
ashutosh1.pandey.ext <ashutosh1.pandey.ext@orange.com>
2023-05-18 16:21:34 +0530

    18-05-2023

---
+++ Python-intro/Python-Terminology.py
@@ -0,0 +1,16 @@
+################################################ Keywords and Variables #########################################################
+
+# 1) Keywords which called reserve words ex - for, while, True, False, if, else, del, elif, and, or, not etc
+# 2) Variables - Is to store some data and have a reference to the location in the variable x=5 5 is stored in some memory location and x is variable is which gives referencing to its 5 memror location
+# 3) Functions - A sequence of instructions that perform a specific task ex- 
+
+print ("ashu") #print function
+input()  #input function
+x = input() 
+
+# 4) Class and Objects --> When you design a slightly larger software you have to use a paradignm called OOP objects oriented programming.
+
+#--> OOPS is where we revolves around object and classes. Where objects are interacting with each other.
+# --> S/W for college classes are = Teacher, Student Classes are blueprint(types) Student class- data of student= name,roll no marks etc
+
+
---
+++ conditions and loops/sumofnum.py
@@ -0,0 +1,9 @@
+n=5
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+    print(sum)
+
+a=5
+sum2 = (a*(a+1))//2
+print (sum2)
